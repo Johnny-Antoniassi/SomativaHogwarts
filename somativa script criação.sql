449c149ec4d673a352cf98b287e6ca6f1c75808f
Johnny-Antoniassi <126523949+Johnny-Antoniassi@users.noreply.github.com>
2023-06-03 17:54:49 -0300

    Add files via upload

---
+++ somativa script criação.sql
@@ -0,0 +1,399 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: formativaHogwarts
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `checkIn`
+--
+
+DROP TABLE IF EXISTS `checkIn`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `checkIn` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `eventoFk` bigint NOT NULL,
+  `usuarioFk` bigint NOT NULL,
+  `data` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `eventoFk` (`eventoFk`),
+  KEY `usuarioFk` (`usuarioFk`),
+  CONSTRAINT `checkIn_ibfk_1` FOREIGN KEY (`eventoFk`) REFERENCES `eventos` (`id`),
+  CONSTRAINT `checkIn_ibfk_2` FOREIGN KEY (`usuarioFk`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `checkIn`
+--
+
+LOCK TABLES `checkIn` WRITE;
+/*!40000 ALTER TABLE `checkIn` DISABLE KEYS */;
+INSERT INTO `checkIn` VALUES (1,1,1,'2023-05-23 00:21:13'),(2,1,2,'2023-05-23 00:21:13'),(3,1,5,'2023-05-23 00:21:13'),(4,2,2,'2023-05-23 00:21:13'),(5,2,4,'2023-05-23 00:21:13'),(6,2,5,'2023-05-23 00:21:13'),(7,2,6,'2023-05-23 00:21:13'),(8,3,1,'2023-05-23 00:21:13'),(9,3,6,'2023-05-23 00:21:13'),(10,4,3,'2023-05-23 00:21:13'),(11,4,4,'2023-05-23 00:21:13'),(12,4,5,'2023-05-23 00:21:13'),(13,1,5,'2023-05-23 01:40:58'),(14,1,6,'2023-05-23 01:40:58');
+/*!40000 ALTER TABLE `checkIn` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `checkList`
+--
+
+DROP TABLE IF EXISTS `checkList`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `checkList` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `localFk` bigint NOT NULL,
+  `itemFk` bigint NOT NULL,
+  `qtd` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `localFk` (`localFk`),
+  KEY `itemFk` (`itemFk`),
+  CONSTRAINT `checkList_ibfk_1` FOREIGN KEY (`localFk`) REFERENCES `locais` (`id`),
+  CONSTRAINT `checkList_ibfk_2` FOREIGN KEY (`itemFk`) REFERENCES `item` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `checkList`
+--
+
+LOCK TABLES `checkList` WRITE;
+/*!40000 ALTER TABLE `checkList` DISABLE KEYS */;
+INSERT INTO `checkList` VALUES (1,1,1,1),(2,1,2,1),(3,1,5,1),(4,2,1,1),(5,2,5,1),(6,3,3,1),(7,3,2,1),(8,4,1,1),(9,4,2,1),(10,4,3,1),(11,4,5,1);
+/*!40000 ALTER TABLE `checkList` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `eventos`
+--
+
+DROP TABLE IF EXISTS `eventos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `eventos` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  `localFk` bigint NOT NULL,
+  `inicio` datetime NOT NULL,
+  `fim` datetime NOT NULL,
+  `inicioCheckIn` datetime NOT NULL,
+  `fimCheckIn` datetime NOT NULL,
+  `vagas` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `localFk` (`localFk`),
+  CONSTRAINT `eventos_ibfk_1` FOREIGN KEY (`localFk`) REFERENCES `locais` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `eventos`
+--
+
+LOCK TABLES `eventos` WRITE;
+/*!40000 ALTER TABLE `eventos` DISABLE KEYS */;
+INSERT INTO `eventos` VALUES (1,'Workshop Cloud Básico',5,'2023-07-07 18:45:00','2023-07-07 23:10:00','2023-06-07 18:45:00','2023-07-07 18:45:00',32),(2,'Desenho Técnico SolidWorks',5,'2023-08-01 18:45:00','2023-08-02 23:10:00','2023-06-08 18:45:00','2023-08-01 18:45:00',32),(3,'Ensaios Mecânicos Avançados',4,'2023-12-01 18:45:00','2023-12-02 23:10:00','2023-12-08 18:45:00','2023-12-01 18:45:00',30),(4,'Conquistando o primeiro milhão',2,'2023-12-12 18:45:00','2023-12-12 23:10:00','2023-12-12 18:45:00','2023-12-12 18:45:00',100),(5,'Como fazer ovo de pascoa',1,'2023-01-01 18:45:00','2023-01-01 23:10:00','2023-01-01 18:45:00','2023-01-01 18:45:00',16);
+/*!40000 ALTER TABLE `eventos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `imagens_status`
+--
+
+DROP TABLE IF EXISTS `imagens_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `imagens_status` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `image` mediumtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `imagens_status`
+--
+
+LOCK TABLES `imagens_status` WRITE;
+/*!40000 ALTER TABLE `imagens_status` DISABLE KEYS */;
+INSERT INTO `imagens_status` VALUES (1,'imagem 11'),(2,'imagem 12'),(3,'imagem 13'),(4,'imagem 14');
+/*!40000 ALTER TABLE `imagens_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'Projetor'),(2,'Ar condicionado'),(3,'Lousa digital'),(4,'Home Theater'),(5,'Ipad'),(6,'Ferro de Solda');
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `locais`
+--
+
+DROP TABLE IF EXISTS `locais`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `locais` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  `bloco` enum('A','B','C','D') NOT NULL,
+  `lotacao` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locais`
+--
+
+LOCK TABLES `locais` WRITE;
+/*!40000 ALTER TABLE `locais` DISABLE KEYS */;
+INSERT INTO `locais` VALUES (1,'Laboratório Eletrônica 01','A',16),(2,'Auditório','C',100),(3,'Laboratório Eletrônica 02','B',16),(4,'Laboratório Mecânica 01','A',30),(5,'Laboratório Informática 01','D',32);
+/*!40000 ALTER TABLE `locais` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nivelAcesso`
+--
+
+DROP TABLE IF EXISTS `nivelAcesso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nivelAcesso` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nivel` varchar(100) NOT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nivelAcesso`
+--
+
+LOCK TABLES `nivelAcesso` WRITE;
+/*!40000 ALTER TABLE `nivelAcesso` DISABLE KEYS */;
+INSERT INTO `nivelAcesso` VALUES (1,'Admin','blablá'),(2,'Gestor','blablá'),(3,'Usuário','blablá'),(4,'Visitante','blablá');
+/*!40000 ALTER TABLE `nivelAcesso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ocupacao`
+--
+
+DROP TABLE IF EXISTS `ocupacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ocupacao` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(150) NOT NULL,
+  `nivelAcessoFk` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `nivelAcessoFk` (`nivelAcessoFk`),
+  CONSTRAINT `ocupacao_ibfk_1` FOREIGN KEY (`nivelAcessoFk`) REFERENCES `nivelAcesso` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ocupacao`
+--
+
+LOCK TABLES `ocupacao` WRITE;
+/*!40000 ALTER TABLE `ocupacao` DISABLE KEYS */;
+INSERT INTO `ocupacao` VALUES (1,'Professor',3),(2,'Secretaria',3),(3,'Coordenador',2),(4,'Orientador',2),(5,'Aluno',4),(6,'TI',1);
+/*!40000 ALTER TABLE `ocupacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `progressos`
+--
+
+DROP TABLE IF EXISTS `progressos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `progressos` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `descrisao` varchar(100) NOT NULL,
+  `data` datetime NOT NULL,
+  `imagens_status_FK` bigint NOT NULL,
+  `tarefa_FK` bigint NOT NULL,
+  `status_FK` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tarefa_FK` (`tarefa_FK`),
+  KEY `status_FK` (`status_FK`),
+  KEY `imagens_status_FK` (`imagens_status_FK`),
+  CONSTRAINT `progressos_ibfk_1` FOREIGN KEY (`tarefa_FK`) REFERENCES `tarefa` (`id`),
+  CONSTRAINT `progressos_ibfk_2` FOREIGN KEY (`status_FK`) REFERENCES `status` (`id`),
+  CONSTRAINT `progressos_ibfk_3` FOREIGN KEY (`imagens_status_FK`) REFERENCES `imagens_status` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `progressos`
+--
+
+LOCK TABLES `progressos` WRITE;
+/*!40000 ALTER TABLE `progressos` DISABLE KEYS */;
+INSERT INTO `progressos` VALUES (1,'pronto','2023-01-30 00:00:00',1,1,1),(2,'ta quase','2023-02-12 00:00:00',2,1,2),(3,'*','2023-03-13 00:00:00',3,1,3),(4,'*','2023-04-14 00:00:00',3,1,4),(5,'cabo','2023-05-15 00:00:00',3,2,1),(6,'*','2023-06-04 00:00:00',1,3,1),(7,'ta quase','2023-07-13 00:00:00',2,4,1),(8,'*','2023-08-16 00:00:00',3,6,1),(9,'*','2023-09-11 00:00:00',3,7,1),(10,'cabo','2023-10-18 00:00:00',3,8,1),(11,'pronto','2023-01-30 00:00:00',1,1,1),(12,'pronto','2023-02-12 00:00:00',2,1,2),(13,'pronto','2023-03-13 00:00:00',3,1,3),(14,'pronto','2023-04-14 00:00:00',3,1,4),(15,'pronto','2023-05-15 00:00:00',3,2,1),(16,'finalizando','2023-06-04 00:00:00',1,3,1),(17,'finalizando','2023-07-13 00:00:00',2,4,1),(18,'finalizando','2023-08-16 00:00:00',3,6,1),(19,'finalizando','2023-09-11 00:00:00',3,7,1),(20,'finalizando','2023-10-18 00:00:00',3,8,1);
+/*!40000 ALTER TABLE `progressos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `responsavel`
+--
+
+DROP TABLE IF EXISTS `responsavel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `responsavel` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `responsavel_fk` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `responsavel_fk` (`responsavel_fk`),
+  CONSTRAINT `responsavel_ibfk_1` FOREIGN KEY (`responsavel_fk`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `responsavel`
+--
+
+LOCK TABLES `responsavel` WRITE;
+/*!40000 ALTER TABLE `responsavel` DISABLE KEYS */;
+INSERT INTO `responsavel` VALUES (1,1),(4,1),(2,2),(5,2),(3,3),(6,3);
+/*!40000 ALTER TABLE `responsavel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `status` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` VALUES (1,'aberta'),(2,'Em andamento'),(3,'Concluida'),(4,'Encerrada');
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tarefa`
+--
+
+DROP TABLE IF EXISTS `tarefa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tarefa` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(50) NOT NULL,
+  `descrisao` varchar(500) NOT NULL,
+  `usuarioFk` bigint NOT NULL,
+  `responsavel_fk` bigint NOT NULL,
+  `prazo` date NOT NULL,
+  `locais_FK` bigint NOT NULL,
+  `imagens` mediumtext,
+  PRIMARY KEY (`id`),
+  KEY `usuarioFk` (`usuarioFk`),
+  KEY `responsavel_fk` (`responsavel_fk`),
+  KEY `locais_FK` (`locais_FK`),
+  CONSTRAINT `tarefa_ibfk_1` FOREIGN KEY (`usuarioFk`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `tarefa_ibfk_2` FOREIGN KEY (`responsavel_fk`) REFERENCES `responsavel` (`id`),
+  CONSTRAINT `tarefa_ibfk_3` FOREIGN KEY (`locais_FK`) REFERENCES `locais` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tarefa`
+--
+
+LOCK TABLES `tarefa` WRITE;
+/*!40000 ALTER TABLE `tarefa` DISABLE KEYS */;
+INSERT INTO `tarefa` VALUES (1,'computador','sem cabos',1,2,'2023-05-10',1,'imagem 1'),(2,'computador','quebrado',6,1,'2023-06-10',2,'imagem 2'),(3,'monitor','quebrado',4,3,'2023-07-12',5,'imagem 3'),(4,'monitor','falha',6,3,'2023-08-11',4,'imagem 4'),(5,'monitor','cabo',4,3,'2023-09-09',2,'imagem 5'),(6,'computador','cabo',4,3,'2023-10-12',2,'imagem 6'),(7,'tablet','carregador',4,3,'2023-11-05',2,'imagem 7'),(8,'celular','fonte',4,3,'2023-01-23',3,'imagem 8'),(9,'luminaria','cabo',4,3,'2023-02-24',4,'imagem 9'),(10,'ar condicionado','tomada',4,3,'2023-03-22',5,'imagem 10');
+/*!40000 ALTER TABLE `tarefa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `dataNascimento` date NOT NULL,
+  `senha` varchar(30) NOT NULL,
+  `dataCadastro` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ocupacaoFk` bigint NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `image` varchar(200) DEFAULT NULL,
+  `cellNumber` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ocupacaoFk` (`ocupacaoFk`),
+  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`ocupacaoFk`) REFERENCES `ocupacao` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'João','joao@gmail.com','2000-01-01','55s888ff','2023-05-22 23:56:10',1,1,NULL,NULL),(2,'Mara','mara@gmail.com','1986-01-03','554333','2023-05-22 23:56:10',2,1,NULL,NULL),(3,'Clarice','clarice@gmail.com','1999-03-01','54455s888ff','2023-05-22 23:56:10',3,1,NULL,NULL),(4,'Roberto','roberto@gmail.com','2001-01-21','6666444','2023-05-22 23:56:10',4,1,NULL,NULL),(5,'Miguel','miguel@gmail.com','1995-03-03','3354','2023-05-22 23:56:10',1,1,NULL,NULL),(6,'Lúcia','lucia@gmail.com','1970-05-25','abbbcdd','2023-05-22 23:56:10',2,1,NULL,NULL);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-03 14:46:49
